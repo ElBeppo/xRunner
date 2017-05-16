f4c832db6f8437804e0db344dd0eaefae417dd90
ElBeppo <eamarkel@gmail.com>
2017-05-16 19:01:38 +0200

    ganando puntos

---
+++ xRuner/Assets/Scripts/Bloque.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque : MonoBehaviour {
+
+	private bool Colision= false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D colision){
+		if(!Colision && (colision.collider.gameObject.name=="PieIzquierdo" || colision.collider.gameObject.name=="PieDerecho")){
+			Colision = true;
+			NotificationCenter.DefaultCenter ().PostNotification (this, "IncrementarPuntos", 1);
+			}
+
+	}
+}
---
+++ xRuner/Assets/Scripts/Puntuacion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntuacion : MonoBehaviour {
+
+	private int puntuacion = 	0;
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter ().AddObserver (this, "IncrementarPuntos");
+	}
+
+	void IncrementarPuntos(Notification notificacion){
+		int puntosAIncrementar = (int)notificacion.data;
+		puntuacion += puntosAIncrementar;
+		Debug.Log("Incrementando "+puntosAIncrementar+"puntos. Total ganados: "+puntuacion);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
